a5b88336818fb1d075e4f60f43126a356c102c4c
PiyushM8 <108424451+PiyushM8@users.noreply.github.com>
2024-02-25 16:30:50 -0500

    Update Navbar.jsx
    Add feedback and contact to navbar

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,32 +1,43 @@
 import { Link } from "react-router-dom";
-import "./navbar.css"
+import "./navbar.css";
 
-function Navbar()
-{
-    return (
-        <div className="navbar-cont">
-            <div className="navbar-items-cont">
-                <div className="navbar-comp-info">
-                    <img className="nav-comp-logo" src="https://cdn.discordapp.com/attachments/1024529988210413632/1210397763972632616/globe.png?ex=65ea69c3&is=65d7f4c3&hm=53c276b3830b969b017a672512620f2aa2d2e12562dc4b37414457e3df55dbf8&"/>
-                    <div className="navbar-name-">
-                        <h3 className="navbar-comp-name">Markcom</h3>
-                        <span className="navbar-moto">OUR SLOGAN</span>
-                    </div>
-                </div>
-                <div className="navbar-extra-cont">
-                    <Link to={"/"}>
-                        <div>Home</div>
-                    </Link>
-                    <Link to={"/register"}>
-                        <div>Register</div>
-                    </Link>
-                    <Link to={"/login"}>
-                        <div>Login</div>
-                    </Link>
-                </div>
-            </div>
+function Navbar() {
+  return (
+    <div className="navbar-cont">
+      <div className="navbar-items-cont">
+        <div className="navbar-comp-info">
+          <img
+            className="nav-comp-logo"
+            src="https://cdn.discordapp.com/attachments/1024529988210413632/1210397763972632616/globe.png?ex=65ea69c3&is=65d7f4c3&hm=53c276b3830b969b017a672512620f2aa2d2e12562dc4b37414457e3df55dbf8&"
+            alt="Company Logo"
+          />
+          <div className="navbar-name-">
+            <h3 className="navbar-comp-name">Markcom</h3>
+            <span className="navbar-moto">OUR SLOGAN</span>
+          </div>
         </div>
-    )
+        <div className="navbar-extra-cont">
+          <Link to={"/"}>
+            <div>Home</div>
+          </Link>
+          <Link to={"/register"}>
+            <div>Register</div>
+          </Link>
+          <Link to={"/login"}>
+            <div>Login</div>
+          </Link>
+          {/* Add Feedback link */}
+          <Link to={"/feedback"}>
+            <div>Feedback</div>
+          </Link>
+          {/* Add Contact link */}
+          <Link to={"/contact"}>
+            <div>Contact</div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;

2fa0a4dd463be66f91d80a66fd0b461ccbc16228
PiyushM8 <108424451+PiyushM8@users.noreply.github.com>
2024-02-25 21:09:25 -0500

    Implement Feedback

--- src/component/homepage/homepage.jsx
+++ src/component/homepage/homepage.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "../auth/Login";
 import Register from "../auth/Register";
+import Feedback from "../feedback/feedback";
 import Navbar from "../navbar/Navbar";
 import Landing from "./landing";
 
@@ -13,6 +14,7 @@ function Homepage()
                 <Route path="/" element={<Landing/>}/>
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/login" element={<Login/>}/>
+                <Route path="/feedback" element={<Feedback/>}/>
             </Routes>
             <div>
                 

2790bd9d065343b274c4ca8fc2dedde744bb374a
PiyushM8 <malip@farmingdale.edu>
2024-03-06 15:31:36 -0500

    Product displayed on storefront

--- src/component/storefront/products/productPage.jsx
+++ src/component/storefront/products/productPage.jsx
@@ -5,7 +5,9 @@ function ProductPage({products})
     return (<div>
         <h2>Products</h2>
         <div className="products-cont">
-            
+        {products.map(product => {
+                return <Product product={product}/>
+            })}
         </div>
     </div>)
 }

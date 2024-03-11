d6cc46361a2769b5c11da14eb884fed2df3c36a9
Daniel Goldstein <124852013+FlyingFladoodle@users.noreply.github.com>
2024-03-11 04:49:50 -0400

    updated product boxes

--- src/component/storefront/products/product.jsx
+++ src/component/storefront/products/product.jsx
@@ -4,11 +4,11 @@ function Product({product})
 {
     return (
         <div className="store-product-item-cont">
-            <div><b>Title:</b> {product.Title}</div>
-            <div><b>Price:</b> {product.Price}</div>
-            <div><b>Description:</b> {product.Description}</div>
-            <div><b>Stock:</b> {product.Stock}</div>
-            <button>Purchase - ${(product.Price).toFixed(2)}</button>
+            <div className="product-text-cont"><b>Title:</b> {product.Title}</div>
+            <div className="product-text-cont"><b>Price:</b> {product.Price}</div>
+            <div className="product-text-cont"><b>Description:</b> {product.Description}</div>
+            <div className="product-text-cont"><b>Stock:</b> {product.Stock}</div>
+            <div className="product-text-cont"><button>Purchase - ${(product.Price).toFixed(2)}</button></div>
         </div>
     )
 }

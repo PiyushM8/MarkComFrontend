7b9c456b8e571313bc5d1c922464f7fd773a944d
PiyushM8 <malip@farmingdale.edu>
2024-05-08 01:06:18 -0400

    Updating customer dashboard style

--- src/component/customerDashboard/customerDashboard.jsx
+++ src/component/customerDashboard/customerDashboard.jsx
@@ -4,52 +4,54 @@ import CustomerInvoices from "./invoices/customerInvoices";
 import { showLogin } from "../../utils/loginregister";
 import Queries from "../dashboard/queries/queries";
 import { signout } from "../../utils/signout";
+import "./customerDashboard.css"; 
 
-function CustomerPage()
-{
-    let navigate = useNavigate()
+function CustomerPage() {
+  let navigate = useNavigate();
 
-    useEffect(() => {
-        const jwtToken = window.sessionStorage.getItem("Authorization")
-        const user = JSON.parse(window.sessionStorage.getItem("UserDetails"));
+  useEffect(() => {
+    const jwtToken = window.sessionStorage.getItem("Authorization");
+    const user = JSON.parse(window.sessionStorage.getItem("UserDetails"));
 
-        if (!jwtToken) {
-            navigate("/")
-            showLogin();
-        }else{
-            if(user.AccountType === "Merchant")
-            {
-                navigate("/dashboard");
-            }
-        }
-    }, [])
+    if (!jwtToken) {
+      navigate("/");
+      showLogin();
+    } else {
+      if (user.AccountType === "Merchant") {
+        navigate("/dashboard");
+      }
+    }
+  }, []);
 
-    return (
-        <div className="dashboard-cont">
-            <div className="dashboard-top-cont">
-                <div className="column-one"></div>
-                <div className="column-two"></div>
-                <div className="column-three"></div>
-            </div>
-            <div className="dashboard-main-cont">
-                <div className="column-one dashboard-nav">
-                    <Link to={"/customer/orders"} className="db-n-item">
-                        Orders
-                    </Link>
-                    <Link to={"/customer/messages"} className="db-n-item">
-                        Messages
-                    </Link>
-                    <button onClick={signout}>Sign Out</button>
-                </div>
-                <div className="column-two dashboard-page-cont">
-                    <Routes>
-                        <Route path='/orders' element={<CustomerInvoices/>}/>
-                        <Route path='/messages/*' element={<Queries/>}/>
-                    </Routes>
-                </div>
-            </div>
+  return (
+    <div className="dashboard-cont">
+      <div className="dashboard-top-cont">
+        <div className="column-one"></div>
+        <div className="column-two"></div>
+        <div className="column-three"></div>
+        {/* Apply the signout button with the signout-btn class */}
+        <button className="signout-btn" onClick={signout}>
+          Sign Out
+        </button>
+      </div>
+      <div className="dashboard-main-cont">
+        <div className="column-one dashboard-nav">
+          <Link to={"/customer/orders"} className="db-n-item">
+            Orders
+          </Link>
+          <Link to={"/customer/messages"} className="db-n-item">
+            Messages
+          </Link>
         </div>
-    )
+        <div className="column-two dashboard-page-cont">
+          <Routes>
+            <Route path="/orders" element={<CustomerInvoices />} />
+            <Route path="/messages/*" element={<Queries />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CustomerPage;

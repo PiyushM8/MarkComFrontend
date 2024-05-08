34b400ff643f3b03175807a59e1207956c116644
PiyushM8 <malip@farmingdale.edu>
2024-05-08 01:17:14 -0400

    contact form update

--- src/component/storefront/queries/contactForm.jsx
+++ src/component/storefront/queries/contactForm.jsx
@@ -1,12 +1,21 @@
-import "./contact.css"
+import { useLocation } from "react-router-dom";
+import "./contact.css"; // Import the CSS file for styling
 
-function ContactForm()
-{
-    return (
-        <div className="store-contact-page-container">
-            <h1>Contact us at... (Support Email)</h1>
-        </div>
-    )
+const GetStoreNameFromURL = () => {
+  const location = useLocation();
+  const urlParts = location.pathname.split("/");
+  return urlParts.length > 1 ? urlParts[1] : ""; // Return the second part of the URL path if it exists, otherwise return an empty string
+};
+
+function ContactForm() {
+  const storeName = GetStoreNameFromURL();
+  const supportEmail = `${storeName}@gmail.com`;
+
+  return (
+    <div className="store-contact-page-container">
+      <h1>Contact us at {supportEmail}</h1>
+    </div>
+  );
 }
 
-export default ContactForm
+export default ContactForm;

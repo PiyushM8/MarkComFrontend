c72a2abf56aacbe80d97e1f48c89bd7ac7b1eb22
PiyushM8 <malip@farmingdale.edu>
2024-05-10 20:42:00 -0400

    Using window location to handle invoice previews

--- src/component/customerDashboard/invoices/invoicePreview.jsx
+++ src/component/customerDashboard/invoices/invoicePreview.jsx
@@ -1,32 +1,35 @@
-import { Link } from "react-router-dom"
+import React from "react";
+import "./invoicePreview.css";
 
 function CustomerInvoicePreview({ invoice }) {
-    return (<div className='cd-invoice-preview'>
-        <Link to={`/${invoice.Username}/order/${invoice.InvoiceId}`} className='cd-invoice-preview-header'>
-            <div className="cd-invoice-info-cont">
-                <img className="cd-invoice-img-preview" src={`https://imagedelivery.net/BMDilndsvZPipd90__49rQ/${invoice.ProductImage}/public`}/>
-                <div className='cd-invoice-p-title-and-id'>
-                    <div className='cd-invoice-p-title'>
-                        {invoice.Quantity}x - {invoice.Title}
-                    </div>
-                    <div className='cd-invoice-p-id'>
-                        Order #: {invoice.InvoiceId}
-                    </div>
-                </div>
+  const handleViewInvoice = () => {
+    window.location.href = `/${invoice.Username}/order/${invoice.InvoiceId}`;
+  };
+
+  return (
+    <div className="cd-invoice-preview">
+      <div className="cd-invoice-preview-header" onClick={handleViewInvoice}>
+        <div className="cd-invoice-info-cont">
+          <img
+            className="cd-invoice-img-preview"
+            src={`https://imagedelivery.net/BMDilndsvZPipd90__49rQ/${invoice.ProductImage}/public`}
+            alt="Product preview"
+          />
+          <div className="cd-invoice-p-title-and-id">
+            <div className="cd-invoice-p-title">
+              {invoice.Quantity}x - {invoice.Title}
             </div>
-            <div className='cd-invoice-p-total'>
-                <div className='cd-invoice-p-total-header'>
-                    Total
-                </div>
-                <div className='cd-invoice-p-total-price'>
-                    ${invoice.InvoicePrice}
-                </div>
-            </div>
-            <div className='cd-invoice-p-status'>
-                {invoice.InvoiceStatus}
-            </div>
-        </Link>
-    </div>)
+            <div className="cd-invoice-p-id">Order #: {invoice.InvoiceId}</div>
+          </div>
+        </div>
+        <div className="cd-invoice-p-total">
+          <div className="cd-invoice-p-total-header">Total</div>
+          <div className="cd-invoice-p-total-price">${invoice.InvoicePrice}</div>
+        </div>
+        <div className="cd-invoice-p-status">{invoice.InvoiceStatus}</div>
+      </div>
+    </div>
+  );
 }
 
-export default CustomerInvoicePreview
+export default CustomerInvoicePreview;
